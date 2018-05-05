231c942affb6ed9ef0fe31fe19fec50d515b64d1
Vito <em14Vito@users.noreply.github.com>
2018-05-05 23:28:11 +0800

    添加config文件

---
+++ bus/spiders/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+url = 'http://bsth.tech:8000'
+
+uri_bus_name = url + '/palmbus_serv/PalmBusJgj/getLineInfoByName.do'
+
+uri_line_name = url + '/palmbus_serv/PalmBusJgj/getLine.do'
+
+uri_stop_detail_info = url + '/palmbus_serv/PalmBusJgj/carMonitor.do'
