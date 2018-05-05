b2a2c6d55bbc3102abbed6650cf5c8ff2052b486
Vito <em14Vito@users.noreply.github.com>
2018-05-05 23:19:03 +0800

    添加DAL模板文件与测试单元

--- bus/spiders/test.py
+++ bus/spiders/test.py
@@ -1,11 +1,30 @@
 # -*- coding: UTF-8 -*-
 import requests
+import bus.spiders.config as R
+from lxml import etree
+uri = R.uri_bus_name
 
-uri = 'http://180.166.5.82:8000/palmbus_serv/PalmBusJgj/getLineInfoByName.do'#?linename=583%E8%B7%AF"
-
-bus_info = {'linename':'583路'}
+bus_info = {'linename':'782路'}
 
 #TODO 添加头
 res = requests.get(uri,params=bus_info)
 print(res.content.decode("utf-8"))
+data = res.content
+# data = '''
+#         <linedetail>
+#             <end_earlytime>05:30</end_earlytime>
+#             <end_latetime>22:30</end_latetime>
+#             <end_stop>懿行路和炯路</end_stop>
+#             <line_id>10225</line_id>
+#             <line_name>583路</line_name>
+#             <start_earlytime>05:30</start_earlytime>
+#             <start_latetime>22:30</start_latetime>
+#             <start_stop>陆家嘴</start_stop>
+#         </linedetail>
+#
+#        '''
+
+parse_data = etree.HTML(data)
+end_last_time = parse_data.xpath('//linedetail/end_stop/text()')
+print(end_last_time)
 
---
+++ bus/utils/db/accessDBDAL.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+#TODO 读取DB数据库信息
